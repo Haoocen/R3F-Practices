690e99188527b6e5174520f3cb0b973b0019800e
Haocen Jiang <haocen9902@gmail.com>
2021-01-04 18:15:58 -0500

    clean up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,30 @@
+import {Container, Grid, makeStyles, Paper} from '@material-ui/core';
 import React from 'react';
+import {allExamples} from "./examplesUtil";
+import {Link} from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        height: 64,
+        width: '100%',
+    }
+}));
 
 const Home: React.FC = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            Home
-        </div>
+        <Container maxWidth="lg">
+            <Grid item xs={12}>
+                <Grid container justify="center" spacing={2}>
+                    {allExamples.map(({path, description}, index) => (
+                        <Grid key={path} item>
+                            <Link to={path}>Example {index+1}</Link>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
